926d7d506f6c4616b673fe6a6e20485cc93ddf58
Joe Cuthbert <josiah.thomas.cuthbert@gmail.com>
2021-01-12 15:29:32 -0600

    order by ex complete

---
+++ order_by_exercises.sql
@@ -0,0 +1,31 @@
+use employees;
+
+SELECT *
+FROM employees
+WHERE gender = 'M' AND first_name = 'Irena'
+OR gender = 'M' AND first_name = 'Vidya'
+OR gender = 'M' AND first_name = 'Maya'
+ORDER BY last_name DESC, first_name DESC;
+
+
+SELECT *
+FROM employees
+WHERE last_name like 'E%'
+AND last_name like '%E'
+ORDER BY emp_no DESC;
+
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31';
+
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25%'
+AND hire_date BETWEEN '1990-1-1' AND '1999-12-31'
+ORDER BY birth_date, hire_date DESC;
+
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
